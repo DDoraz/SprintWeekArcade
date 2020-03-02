00b024695a8254544004fd3044aba1e9b6592a5b
AaronMunavishHogarth <munavishhogarth@gmail.com>
2020-03-02 15:01:13 -0500

    Unity Event added to call punch attack
    - When the player comes into range of the enemy, the enemy will throw a punch

--- Sprint Week Fighters/Assets/Scripts/AaronScripts/EnemyAI.cs
+++ Sprint Week Fighters/Assets/Scripts/AaronScripts/EnemyAI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -11,22 +12,27 @@ public class EnemyAI : MonoBehaviour
     public float f_damage;
 
     //The target transform for the player
-    private Transform target;
+    private Transform t_Target;
 
     // Start is called before the first frame update
     void Start()
     {
         //Gets the transform of the gameobject tagged player
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        t_Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(target.position.x <= 3f || target.position.x >= -3f)
+        //When the player comes in range of the enemy, the enemy will stop
+        if(t_Target.position.x <= t_Target.position.x + 2)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, f_speed * Time.deltaTime);
-
+            transform.position = Vector2.MoveTowards(transform.position, t_Target.position, f_speed * Time.deltaTime);
         }
+    }
+
+    void PunchAttack()
+    {
+        Debug.Log("A punch was thrown");
     }
 }
