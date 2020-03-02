302854cd283cd68d61d70bee8fc4c643dc812fc1
AaronMunavishHogarth <munavishhogarth@gmail.com>
2020-03-02 14:45:51 -0500

    Simple AI follow prototype added
    - Added a punch trigger box for the enemy

--- Sprint Week Fighters/Assets/Scripts/AaronScripts/EnemyAI.cs
+++ Sprint Week Fighters/Assets/Scripts/AaronScripts/EnemyAI.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public float f_speed;
+
+    public float f_health;
+
+    public float f_damage;
+
+    //The target transform for the player
+    private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Gets the transform of the gameobject tagged player
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(target.position.x <= 3f || target.position.x >= -3f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, f_speed * Time.deltaTime);
+
+        }
     }
 }
