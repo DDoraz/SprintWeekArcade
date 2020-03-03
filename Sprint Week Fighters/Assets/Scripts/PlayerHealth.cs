c0b24a23d4e2dde3dc1b999049a5c5316d26a386
DominusNocte73 <dookrabe@sheridancollege.ca>
2020-03-03 11:01:46 -0500

    commit

---
+++ Sprint Week Fighters/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    bool deathCheck;
+    bool damaged;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+
+        }
+
+        else
+        {
+
+        }
+
+        damaged = false;
+    }
+
+    public void TakeDamage()
+    {
+        int amount = 5;
+
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        if(currentHealth <= 0 && !deathCheck)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        deathCheck = true;
+    }
+}
