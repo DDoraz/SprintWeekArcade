7bd3403f23234faedbccfcd6ae2ff185205c5e8a
David <daviddorazio8@gmail.com>
2020-03-03 01:00:07 -0500

    Movement and Punch
    Melee needs stuff added still

---
+++ Sprint Week Fighters/Assets/Scripts/Enemies.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    public int maxHouse = 100;
+    int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHouse;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+
+    }
+}
