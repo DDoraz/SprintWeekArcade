f7b440d30fe43d6d940e060c20c531c714fc83ab
ShadowHaxor99 <chrisfarmer28@gmail.com>
2020-03-04 15:20:01 -0500

    added text and templete for scene changing

---
+++ Sprint Week Fighters/Assets/Scripts/TitleText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TitleText : MonoBehaviour
+{
+    private TextMeshPro coinText;
+    public Color lerpColor;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        TextMeshPro coinText = GetComponent<TextMeshPro>();
+        Color lerpColor = new Color();
+        coinText.color = lerpColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lerpColor = Color.Lerp(Color.yellow, Color.white, Mathf.PingPong(Time.time, 1));
+    }
+}
